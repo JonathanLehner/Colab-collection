6e4adc1b92d39fdd09669b923ddcd89b604bd4d9
JonathanLehner <lehnerj@student.ethz.ch>
2019-04-29 23:13:35 +0200

    Update products.py

--- products.py
+++ products.py
@@ -401,7 +401,7 @@ if __name__ == '__main__':
 
     # Train or evaluate
     if args.command == "train":
-        train(model,epochs)
+        train(model,args.epochs)
     elif args.command == "splash":
         detect_and_color_splash(model, image_path=args.image,
                                 video_path=args.video)
